2d017a78f43e20cbe624fa67e4a5a8b4aaecdf47
Philipp Schiffmann <philippschiffmann93@gmail.com>
2017-02-02 02:44:50 +0100

    Update Parser actions

--- lib/src/parser.dart
+++ lib/src/parser.dart
@@ -1,17 +1,15 @@
 import 'package:built_collection/built_collection.dart';
+import 'package:tuple/tuple.dart' show Tuple2;
 import 'grammar.dart';
 
 class Parser {
-  /// Referenced by [ExpandAction]s.
-  final BuiltMap<Nonterminal, BuiltList<ParserAction>> expansions;
+  final BuiltList<BuiltList<ParserAction>> actions;
 
-  final ExpandAction startAction;
-
-  Parser(this.expansions, this.startAction);
+  Parser(this.actions);
 
   Object parse(Iterable<Terminal> input) {
-    var context = new ParsingContext(expansions, input.iterator);
-    context.actions.add(startAction);
+    var context = new ParsingContext(this, input.iterator);
+    context.actions.addAll(actions[0]);
 
     while (context.actions.isNotEmpty) {
       context.actions.removeLast().execute(context);
@@ -29,13 +27,15 @@ class Parser {
 }
 
 class ParsingContext {
-  final BuiltMap<Nonterminal, BuiltList<ParserAction>> expansions;
+  final Parser parser;
 
-  Iterator<Terminal> tokens;
-  List reduced = [];
-  List<ParserAction> actions = [];
+  final Iterator<Terminal> tokens;
+  final List reduced = [];
+  final List<ParserAction> actions = [];
+  final List<Tuple2<ParserAction, int>> marked = [];
+  Nonterminal lastReduced;
 
-  ParsingContext(this.expansions, this.tokens);
+  ParsingContext(this.parser, this.tokens);
 }
 
 abstract class ParserAction {
@@ -54,40 +54,60 @@ class ShiftAction extends ParserAction {
     context.reduced.add(context.tokens.current);
     context.tokens.moveNext();
   }
+
+  String toString() => 's($terminal)';
 }
 
 class ExpandAction extends ParserAction {
-  final Nonterminal nonterminal;
+  final int action;
 
-  ExpandAction(this.nonterminal);
+  ExpandAction(this.action);
 
   void execute(ParsingContext context) {
-    context.actions.addAll(context.expansions[nonterminal]);
+    context.actions.addAll(context.parser.actions[action]);
   }
+
+  String toString() => 'e($action)';
 }
 
-class BranchAction extends ParserAction {
-  final Map<Terminal, List<ParserAction>> branches;
+class LookAheadAction extends ParserAction {
+  final BuiltMap<Terminal, int> branches;
 
-  BranchAction(this.branches);
+  LookAheadAction(this.branches);
 
   void execute(ParsingContext context) {
     if (branches.containsKey(context.tokens.current)) {
-      context.actions.addAll(branches[context.tokens.current]);
+      context.actions
+          .addAll(context.parser.actions[branches[context.tokens.current]]);
     } else if (branches.containsKey(Terminal.emptyWord)) {
-      context.actions.addAll(branches[Terminal.emptyWord]);
+      context.actions
+          .addAll(context.parser.actions[branches[Terminal.emptyWord]]);
+    } else {
+      throw new Exception(
+          "Input rejected at token `${context.tokens.current}`");
     }
   }
+
+  String toString() => 'l$branches';
 }
 
-class ReturnAwareBranchAction extends ParserAction {
-  void execute(ParsingContext context) {}
+class ContinueAction extends ParserAction {
+  final BuiltMap<Nonterminal, int> branches;
+
+  ContinueAction(this.branches);
+
+  void execute(ParsingContext context) {
+    context.actions
+        .addAll(context.parser.actions[branches[context.lastReduced]]);
+  }
+
+  String toString() => 'c$branches';
 }
 
-class ConstructAction extends ParserAction {
+class ReduceAction extends ParserAction {
   final Production production;
 
-  ConstructAction(this.production);
+  ReduceAction(this.production);
 
   void execute(ParsingContext context) {
     var node = new AstNode(
@@ -97,7 +117,28 @@ class ConstructAction extends ParserAction {
     context.reduced
       ..length -= node.children.length
       ..add(node);
+
+    context.lastReduced = production.lhs;
+    if (context.marked.isNotEmpty) {
+      while (context.marked.last.item2 > context.reduced.length)
+        context.marked.removeLast();
+      context.actions.add(context.marked.removeLast().item1);
+    }
   }
+
+  String toString() => 'r($production)';
+}
+
+class MarkAction extends ParserAction {
+  final ParserAction action;
+
+  MarkAction(this.action);
+
+  void execute(ParsingContext context) {
+    context.marked.add(new Tuple2(action, context.reduced.length));
+  }
+
+  String toString() => 'm($action)';
 }
 
 class AstNode {
